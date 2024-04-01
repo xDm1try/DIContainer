3bb833cf2818cc4d5eaba3132102e6a8a4f5cf11
Dmitry Muraviev <MuravjevDV@yandex.ru>
2024-04-02 00:48:39 +0700

    returned logging by postprocessor

--- app/src/main/java/ru/nsu/fit/dicontainer/postprocessor/LoggingPostProcessor.java
+++ app/src/main/java/ru/nsu/fit/dicontainer/postprocessor/LoggingPostProcessor.java
@@ -5,6 +5,6 @@ import ru.nsu.fit.dicontainer.annotation.PostConstruct;
 public class LoggingPostProcessor implements BeanPostProcessor {
   @Override
   public void process(Object bean) {
-
+    System.out.println("LoggingPostProcessor: Bean " + bean.hashCode() + "is initialized");
   }
 }
